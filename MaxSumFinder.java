2547cf7718af812aed53651764a46943dc0c8f5a
Artur <screamandko@gmail.com>
2026-05-26 15:06:45 +0300

    feat: add alternative loops solution using loops

--- MaxSumFinder.java
+++ MaxSumFinder.java
@@ -5,18 +5,28 @@ import java.util.Scanner;
 public class MaxSumFinder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int [] numbers = new int [3];
 
         System.out.println("Enter three numbers:");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
 
-        int min = a;
-        if (b < min) min = b;
-        if (c < min) min = c;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
 
-        int result = (a + b + c) - min;
+        for (int num : numbers) {
+            if (num > max2) {
+                max2=max1;
+                max1=num;
 
+            } else if (num > max2) {
+                max2=num;
+
+            }
+        }
+        int result = max1 + max2;
         System.out.println("The sum of the two largest numbers is: " + result);
+
     }
 }
